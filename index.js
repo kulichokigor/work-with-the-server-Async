9a3838e46bf3cf68af8e2c5b340c0a627a914fe6
kulichokigor <igorkulichok@gmail.com>
2020-05-27 14:40:28 +0300

    Work with FETCH

---
+++ index.js
@@ -0,0 +1,18 @@
+const h1 = document.querySelector('h1');
+const button = document.querySelector('#load');
+
+button.addEventListener('click',()=>{
+    const url='https://jsonplaceholder.typicode.com/users'
+    h1.style.color = 'lightseagreen'
+    button.setAttribute('disabled', true)
+    fetch(url)
+        .then(response=>response.json())
+        .then((data)=>{
+            const ul = document.querySelector('#list');
+            ul.style.listStyle="none"
+            const usersList = data.map((item)=>{
+                return `<li><b style="color:#036f56">${item.id}.</b> ${item.name} (${item.username}). Email: ${item.email}</li>`
+            })
+            ul.insertAdjacentHTML('afterbegin', usersList.join(' '));
+        })
+})
