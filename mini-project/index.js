b4ab3f8f29c604bba45b977c27f91b2b99b5bd21
kulichokigor <igorkulichok@gmail.com>
2020-05-28 17:38:39 +0300

    replace then () object with an array. Using the map () method

--- mini-project/index.js
+++ mini-project/index.js
@@ -1,10 +1,22 @@
 const button = document.querySelector('#load');
 const addresIp = document.querySelector('#addres-ip');
 const list = document.querySelector('#list');
+const close = document.querySelector('.close')
+
 
 button.addEventListener('click', ()=>{
     const url1 = 'https://api.ipify.org/?format=json';
     const url2 = 'https://api.ipdata.co/?api-key=test';
+    !list.classList.contains('open')?list.classList.add('open'):NaN
+    button.setAttribute('disabled',true)
+    list.addEventListener('click',event=>{
+        event.target.classList.contains('close')?(
+            list.classList.remove('open'),
+            button.removeAttribute('disabled'),
+            list.innerHTML = '<div class="close"></div>'
+        ):NaN
+    })
+
 
     fetch(url1)
         .then(res=>res.json())
@@ -17,7 +29,7 @@ button.addEventListener('click', ()=>{
     fetch(url2)
         .then(res=>res.json())
         .then((base)=>{
-            return {
+            const userNewList = [{
                 dataUser: new Date().toLocaleString(),
                 country: base.country_name,
                 city:base.city,
@@ -25,21 +37,32 @@ button.addEventListener('click', ()=>{
                 region:base.region,
                 img:base.flag,
                 symbol:base.currency.symbol
-            }
+            }]
+            return userNewList
         })
+
         .then(data=>{
-            
-            for(const key in data){
-              list.innerHTML = 
-              `<div class="item-list">
-                    <div>${data.country}</div>
-                    <div>${data.city}</div>
-                    <div>${data.postal}</div>
-                    <div>${data.region}</div>
-                    <div>${data.symbol}</div>
-                    <div>${data.dataUser}</div>
-                </div>
-              `
-           }
+            const itemList = data.map((item)=>{
+                return `
+                    <div class="conteiner-list">
+                        <h5>${item.dataUser}</h5>
+                        <br/>
+                        <div>${item.country}<div>
+                        <br/>
+                        <div>${item.city}<div>
+                        <br/>
+                        <div>${item.postal}<div>
+                        <br/>
+                        <div>${item.region}<div>
+                        <br/>
+                        <div>${item.symbol}<div>
+                        <br/>
+                        <img src="${item.img}" alt="flag"/>
+                    </div>
+                    `
+            })
+            list.insertAdjacentHTML('afterbegin', itemList)
         })
+
+
 })
